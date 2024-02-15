00ca410f0a5c4c03cdf543eeb8d6ef7ec96f0d38
Ernesto <ernestoar10@hotmail.com>
2024-02-15 10:04:45 -0600

    Agregar javascript

---
+++ intro_js.js
@@ -0,0 +1,59 @@
+// consola (log, info, warn, error, assert)
+console.log("hola");
+console.info("Esto es información");
+console.warn("Esto es un advertencia");
+console.error("Esto es un error");
+console.assert(1==1);
+console.assert(1==2);
+
+
+// variables, constantes
+
+
+
+// Alcance de las variables
+
+
+
+// alert, prompt, confirm
+
+
+// funciones tradicionales
+
+
+// funciones modernas
+
+
+// arreglos
+
+
+//const arreglo = ["Elemento"];
+
+
+//const arreglo = new Array() 
+
+
+//arreglo.push("Otro elemento");
+
+
+//arreglo[10] = "Uno más";
+
+
+//arreglos asociativos
+
+
+//recorrido tradicional del arreglo
+//for (let i = 0; i < arreglo.length; i++) {
+//    console.log(arreglo[i]);
+//}
+
+
+//recorridos alternativos del arreglo
+//for(let posicion in arreglo) {
+//    console.log(posicion);
+//}
+
+
+//Objetos
+//const objeto = {} 
+// modificar html

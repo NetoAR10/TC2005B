064d56971f73324b7398b7e7c5c34124b945e4b8
Ernesto <ernestoar10@hotmail.com>
2024-02-22 10:41:23 -0600

    server

--- hacks.js
+++ hacks.js
@@ -1,4 +1,38 @@
 console.log("hola mundo de maincra");
 
+//fs es el modulo para manipular el sistema de archivos
 const filesystem = require('fs');
+
+//Escribe el segundo parametro en el archivo indicado en el primer parametro
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+const te_hackie = () => {
+    console.log('jojo te hackie');
+};
+
+
+//codigo asincrono, se ejecuta en 7 segundos
+setTimeout(te_hackie, 7000);
+
+const arreglo = [5000, 60, 90, 100, 10, 21, 1000, 0, 120, 2000, 340, 10000];
+
+//codigo asicncrono se imprime en orden
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
+
+console.log("esto se imprime antes de los numeros");
+
+//http modulo de node con todas las funciones de un servidor web
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write("hola mundo desde node");
+    response.end();
+});
+
+server.listen(3000);

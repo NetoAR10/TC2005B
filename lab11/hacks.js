0ba90b6438d5ded8c6d4ee2e98fd7060608502fe
Ernesto <ernestoar10@hotmail.com>
2024-03-03 14:28:44 -0600

    muchos cambios

---
+++ lab11/hacks.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+//registrar el middleware con el modulo construcciones
+const rutasConstrucciones = require('./routes/construcciones.routes');
+
+app.use('/', rutasConstrucciones);
+
+app.use((request, response, next) => {
+    response.status(404);
+    response.sendFile(path.join(__dirname, 'views', '404.html'));
+  });
+
+  app.listen(3000);
